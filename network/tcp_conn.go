074da5b5eb2b99f8380e39c9b576c8d93f7e26a3
eric <592779130@qq.com>
2020-03-09 00:00:19 +0800

    添加network模块中tcp_conn和tcp_msg的注释

--- network/tcp_conn.go
+++ network/tcp_conn.go
@@ -6,23 +6,25 @@ import (
 	"sync"
 )
 
-type ConnSet map[net.Conn]struct{}
+type ConnSet map[net.Conn]struct{}	// 定义一个ConnSet类型实际为一个net.Conn到空对象的映射
 
 type TCPConn struct {
-	sync.Mutex
-	conn      net.Conn
-	writeChan chan []byte
-	closeFlag bool
-	msgParser *MsgParser
+	sync.Mutex					// 互斥锁
+	conn      net.Conn			// 连接
+	writeChan chan []byte		// 写通道
+	closeFlag bool				// 关闭标识符
+	msgParser *MsgParser		// 消息解释器
 }
 
+// 初始化一个TCPConn，pendingWriteNum为写通道的容量
 func newTCPConn(conn net.Conn, pendingWriteNum int, msgParser *MsgParser) *TCPConn {
 	tcpConn := new(TCPConn)
 	tcpConn.conn = conn
 	tcpConn.writeChan = make(chan []byte, pendingWriteNum)
 	tcpConn.msgParser = msgParser
-
+	// 开启一个协程，从writechan中不断的接受数据，然后发送出去
 	go func() {
+		// 当tcpConn关闭时会向tcpConn.writeChan中发送一个nil
 		for b := range tcpConn.writeChan {
 			if b == nil {
 				break
@@ -33,7 +35,7 @@ func newTCPConn(conn net.Conn, pendingWriteNum int, msgParser *MsgParser) *TCPCo
 				break
 			}
 		}
-
+		// 当发送完成后，关闭连接，并且将tcpConn的closeFlag设置为true，表示writeChan已关闭
 		conn.Close()
 		tcpConn.Lock()
 		tcpConn.closeFlag = true
@@ -52,14 +54,14 @@ func (tcpConn *TCPConn) doDestroy() {
 		tcpConn.closeFlag = true
 	}
 }
-
+// tcpConn销毁，如果再销毁时发现writeChan通道未关闭，则关闭通道
 func (tcpConn *TCPConn) Destroy() {
 	tcpConn.Lock()
 	defer tcpConn.Unlock()
 
 	tcpConn.doDestroy()
 }
-
+// 关闭tcpConn，如果writeChan通道未关闭，则会向通道发送nil,这里直接走的doWrite并没有走Write,因为Write会将nil过滤掉，保证其他地方不会传入nil
 func (tcpConn *TCPConn) Close() {
 	tcpConn.Lock()
 	defer tcpConn.Unlock()
@@ -82,6 +84,7 @@ func (tcpConn *TCPConn) doWrite(b []byte) {
 }
 
 // b must not be modified by the others goroutines
+// 通过调用do_write,将信息b发送到writeChan中
 func (tcpConn *TCPConn) Write(b []byte) {
 	tcpConn.Lock()
 	defer tcpConn.Unlock()
@@ -92,22 +95,25 @@ func (tcpConn *TCPConn) Write(b []byte) {
 	tcpConn.doWrite(b)
 }
 
+//
 func (tcpConn *TCPConn) Read(b []byte) (int, error) {
 	return tcpConn.conn.Read(b)
 }
-
+// 获取本地网络地址
 func (tcpConn *TCPConn) LocalAddr() net.Addr {
 	return tcpConn.conn.LocalAddr()
 }
-
+// 获取远程网络地址
 func (tcpConn *TCPConn) RemoteAddr() net.Addr {
 	return tcpConn.conn.RemoteAddr()
 }
-
+// 通过调用MsgParser的Read方法，读取数据。
 func (tcpConn *TCPConn) ReadMsg() ([]byte, error) {
 	return tcpConn.msgParser.Read(tcpConn)
 }
 
+// 先通过msgParser的Write将信息按照协议封装，在msgParser.Wirte中会调用TCPConn的Write，最终实现将封装好的信息发送到
+// writeChan中，详细参考tcp_msg中MsgParser的Write方法
 func (tcpConn *TCPConn) WriteMsg(args ...[]byte) error {
 	return tcpConn.msgParser.Write(tcpConn, args...)
 }
--- network/tcp_msg.go
+++ network/tcp_msg.go
@@ -11,12 +11,12 @@ import (
 // | len | data |
 // --------------
 type MsgParser struct {
-	lenMsgLen    int
-	minMsgLen    uint32
-	maxMsgLen    uint32
-	littleEndian bool
+	lenMsgLen    int		// len的字节长度
+	minMsgLen    uint32		// 最小msg长度
+	maxMsgLen    uint32		// 最大msg长度
+	littleEndian bool		// 大小端
 }
-
+// 创建一个消息解析器
 func NewMsgParser() *MsgParser {
 	p := new(MsgParser)
 	p.lenMsgLen = 2
@@ -27,7 +27,8 @@ func NewMsgParser() *MsgParser {
 	return p
 }
 
-// It's dangerous to call the method on reading or writing
+// 在读写时调用该方法是危险的，lenMsgLen只能是 1 或者 2 或者 4
+// 设置消息解析器的长度参数（len,minLen,maxLen）
 func (p *MsgParser) SetMsgLen(lenMsgLen int, minMsgLen uint32, maxMsgLen uint32) {
 	if lenMsgLen == 1 || lenMsgLen == 2 || lenMsgLen == 4 {
 		p.lenMsgLen = lenMsgLen
@@ -57,21 +58,23 @@ func (p *MsgParser) SetMsgLen(lenMsgLen int, minMsgLen uint32, maxMsgLen uint32)
 }
 
 // It's dangerous to call the method on reading or writing
+// 设置大小端（true 代表小端， false 代表大端）
 func (p *MsgParser) SetByteOrder(littleEndian bool) {
 	p.littleEndian = littleEndian
 }
 
 // goroutine safe
+// 因为conn实现了Read方法，所以Read方法会通过io:ReadFull来读取conn的数据,io:ReadFull最终会调用conn.Read来获取数据,参考io:ReadFull源码
 func (p *MsgParser) Read(conn *TCPConn) ([]byte, error) {
 	var b [4]byte
 	bufMsgLen := b[:p.lenMsgLen]
 
-	// read len
+	// read len  读取len大小的字节
 	if _, err := io.ReadFull(conn, bufMsgLen); err != nil {
 		return nil, err
 	}
 
-	// parse len
+	// parse len 通过解析获取包的长度len
 	var msgLen uint32
 	switch p.lenMsgLen {
 	case 1:
@@ -90,14 +93,14 @@ func (p *MsgParser) Read(conn *TCPConn) ([]byte, error) {
 		}
 	}
 
-	// check len
+	// check len 检查len大小，是不是在规定的min到max之间
 	if msgLen > p.maxMsgLen {
 		return nil, errors.New("message too long")
 	} else if msgLen < p.minMsgLen {
 		return nil, errors.New("message too short")
 	}
 
-	// data
+	// data 读取len大小的数据
 	msgData := make([]byte, msgLen)
 	if _, err := io.ReadFull(conn, msgData); err != nil {
 		return nil, err
@@ -108,22 +111,22 @@ func (p *MsgParser) Read(conn *TCPConn) ([]byte, error) {
 
 // goroutine safe
 func (p *MsgParser) Write(conn *TCPConn, args ...[]byte) error {
-	// get len
+	// get len  获取msg的len
 	var msgLen uint32
 	for i := 0; i < len(args); i++ {
 		msgLen += uint32(len(args[i]))
 	}
 
-	// check len
+	// check len 检测len是否在min和max之间
 	if msgLen > p.maxMsgLen {
 		return errors.New("message too long")
 	} else if msgLen < p.minMsgLen {
 		return errors.New("message too short")
 	}
-
+	// 根据len和data大小申请msg buf
 	msg := make([]byte, uint32(p.lenMsgLen)+msgLen)
 
-	// write len
+	// write len 根据大小端和len的字节长度，向msg buf中写入len
 	switch p.lenMsgLen {
 	case 1:
 		msg[0] = byte(msgLen)
@@ -141,7 +144,7 @@ func (p *MsgParser) Write(conn *TCPConn, args ...[]byte) error {
 		}
 	}
 
-	// write data
+	// write data 将data写入到msg buf中
 	l := p.lenMsgLen
 	for i := 0; i < len(args); i++ {
 		copy(msg[l:], args[i])
